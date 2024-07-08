ebe783c244bd3ca8810fde2c66c8b19bdef74914
vahid <vahid.ufi@gmail.com>
2024-07-08 10:22:44 +0330

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,7 @@
 import './App.css'
 import { LoginForm } from './components/LoginForm/LoginForm'
-import './bootstrap.min.css'
-import { useState } from 'react'
 
 function App() {
-
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [validateMessage, setValidateMessage] = useState('') 
-  const submit = () =>{
-    if(name || email || password === ''){ 
-      setValidateMessage('لطفا مشخصات خود را کامل وارد نمایید')
-    }
-  }
   return (
     <>
       <LoginForm/>
--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,23 +1,26 @@
-import React from 'react'
-import './loginform.css'
+import React, { useState } from 'react'
 export const LoginForm = () => {
+  const [email, setEmail] = useState('')
+  const [name, setName] = useState('')
+  const [password, setPassword] = useState('')
+  const [validateMessage, setValidateMessage] = useState('')
+  
+  const submit = () => {
+    console.log("submit")
+  }
   return (
     <div>
       <section className='w-100 vh-100 d-flex justify-content-center align-items-center' >     
-        <div className='form-body w-50 h-auto'>
+        <div className='card form-body w-50 h-auto p-4'>
           <div className='title-form mt-5 text-center'>
             <h3 className='fs-5 fw-normal'>
               برای ثبت نام مشخصات خود را وارد نمایید و سپس بر روی دکمه ورود کلیک کنید.
             </h3>
           </div>
           <form className='forms w-100 h-auto'>
-            <div>
-              <label className='m-3' htmlFor="inputName">
-                <p className='fs-5'>
-                  نام و نام خانوادگی
-                </p>
-              <input type="name" id='name' onChange={(e) => setName(e.target.value)} value={name}/>
-              </label>
+            <div className="mb-3">
+              <label htmlFor="name" className="form-label">نام و نام خانوادگی</label>
+              <input type="name" id='name' onChange={(e) => setName(e.target.value)} value={name} className="form-control" />
             </div>
 
             <div>
@@ -40,15 +43,15 @@ export const LoginForm = () => {
           </form>
 
           <div className='submit w-100 h-auto text-center m-2'>
-           <button type="button" className='btn btn-secondary w-25 fs-5' onClick={submit}>
-            انجام شد
-           </button>
+            <button type="button" className='btn btn-primary fs-5' onClick={submit}>
+              انجام شد
+            </button>
           </div>
 
           <div>
             {
               validateMessage !== '' ?
-              <div class="alert alert-secondary" role="alert">
+              <div className="alert alert-secondary" role="alert">
                 {validateMessage}
               </div>
               :
